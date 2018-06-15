259091d9f3ba7611f8ababa7bef1f99bbe735d41
Alexis Fortin-Côté <ALFOC@ulaval.ca>
2018-06-15 16:46:40 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+import os
+import json
+import time
+from collections import OrderedDict
+import random
+from flask import Flask, render_template, request, url_for, redirect, flash, session, abort
+
+from forms import *
+
+from webui import WebUI # Add WebUI to your imports
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('secretkey', 'qweqwe1e2e')
+
+ui = WebUI(app, debug=True) # Create a WebUI instance
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  
+    form = SNForm(request.form)
+    if request.method == 'POST' and form.validate():
+        print(form.data)
+        return redirect(url_for('fin'))
+
+    if request.method == 'POST' and not form.validate():
+        for error in form.errors:
+            flash(form.errors.get(error)[-1])
+
+    return render_template("index.html", form=form)
+
+
+@app.route('/fin', methods=['GET'])
+def fin():
+    return render_template("fin.html")
+
+
+if __name__ == '__main__':
+    ui.run() 
+    #app.run(debug=True, port=5000)
